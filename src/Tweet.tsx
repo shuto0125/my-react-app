283b7fe23ee1ae2bfdec8b28b7854aaaa362c653
かいたにしゅうと <shuuto@kaitanishuutonoMacBook-Air.local>
2021-05-31 02:22:27 +0900

    ツイート追加

---
+++ src/Tweet.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+// import ReactDOM from "react-dom";
+
+const Tweet = (props: any) => {
+  const [liked, setLike] = React.useState(false);
+  const toggleLike = React.useCallback(
+    () => setLike((prev) => !prev),
+    [setLike]
+  );
+  return (
+    <div className="tweet">
+      <div className="icon-container">{props.icon}</div>
+      <div className="body-container">
+        <div className="status-display">
+          <span className="display-name">{props.displayName}</span>
+          <span className="account-name">@{props.accountName}</span>
+        </div>
+        <div className="content">{props.content}</div>
+        <div className="status-action">
+          <span className="toggle-like" onClick={toggleLike}>
+            {liked ? "❤️" : "♡"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tweet;
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,40 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./Hello";
+import Tweet from "./Tweet";
+import Timeline from "./Timeline";
+import TweetInput from "./TweetInput";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
-  document.getElementById("app")
-);
+function App() {
+  const [tweets, setTweets] = React.useState([
+    {
+      id: 0,
+      icon: "🌽",
+      displayName: "もろこし太郎",
+      accountName: "morokoshi",
+      content: "今日も1日もろこしがうまい",
+    },
+    {
+      id: 1,
+      icon: "🦐",
+      displayName: "エビデンス",
+      accountName: "evidence",
+      content: "かにみそたべたい",
+    },
+  ]);
+  // addTweet関数はuseCallbackで作成する
+  // これも毎回作成していると重くなるので
+  const addTweet = React.useCallback(
+    (tweet) => setTweets((prev: any) => [tweet, ...prev]),
+    [setTweets]
+  );
+
+  return (
+    <React.StrictMode>
+      <Hello />
+      <TweetInput addTweet={addTweet} />
+      <Timeline tweets={tweets} />
+    </React.StrictMode>
+  );
+}
+ReactDOM.render(<App />, document.getElementById("app"));
